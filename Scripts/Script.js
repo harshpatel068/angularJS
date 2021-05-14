835e3a86efdb0eb980c50709a49fbebbebb2772f
Harsh <17it068@charusat.edu.in>
2021-05-15 01:19:25 +0530

    First web file.

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -4,7 +4,7 @@ var app = angular
                   .module("myModule",[])
                   .controller("myController", function ($scope) {
                     var employees = [
-                      { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000.788 },
+                      { name: "Harsh", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000.788 },
                       { name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender: "female", salary: 68000 },
                       { name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", salary: 57000 },
                       { name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender: "female", salary: 53000 },
